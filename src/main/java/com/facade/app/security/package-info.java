8c0688d11bc878465685ba9b4f3c59131e37fafa
syongkheng <33014950+syongkheng@users.noreply.github.com>
2023-10-01 18:17:08 +0800

    Initial version of facade generated by generator-jhipster@7.9.4

---
+++ src/main/java/com/facade/app/security/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Security configuration.
+ */
+package com.facade.app.security;
---
+++ src/test/java/com/facade/app/IntegrationTest.java
@@ -0,0 +1,24 @@
+package com.facade.app;
+
+import com.facade.app.FacadeApp;
+import com.facade.app.config.AsyncSyncConfiguration;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { FacadeApp.class, AsyncSyncConfiguration.class })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+    // 5s is the spring default https://github.com/spring-projects/spring-framework/blob/29185a3d28fa5e9c1b4821ffe519ef6f56b51962/spring-test/src/main/java/org/springframework/test/web/reactive/server/DefaultWebTestClient.java#L106
+    String DEFAULT_TIMEOUT = "PT5S";
+
+    String DEFAULT_ENTITY_TIMEOUT = "PT5S";
+}
